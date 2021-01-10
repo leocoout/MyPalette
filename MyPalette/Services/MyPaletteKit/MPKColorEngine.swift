1765f24064bf5ca53f9832085323f7e65e8a8a31
Leonardo Coutinho <leonardo.coutinho@br.experian.com>
2021-01-10 17:14:06 -0300

    improvements in the UICOlorToSring conversion in the MPKColorEngine

--- MyPalette/Services/MyPaletteKit/MPKColorEngine.swift
+++ MyPalette/Services/MyPaletteKit/MPKColorEngine.swift
@@ -52,16 +52,9 @@ public class MPKColorEngine {
     /// Converts the hexadecimal value of a UIColor object to a string starting with #
     /// - Returns: the string value
     static func convertUIColorToString(using color: UIColor) -> String {
-        guard let colorRef = color.cgColor.components else { return "Não foi possível detectar a cor" }
-        
-        let r:CGFloat = colorRef[0]
-        let g:CGFloat = colorRef[1]
-        let b:CGFloat = colorRef[2]
-        
-        return String(format: "#%02lX%02lX%02lX",
-                      lroundf(Float(r * 255)),
-                      lroundf(Float(g * 255)),
-                      lroundf(Float(b * 255)))
+        var r: CGFloat = 0, g: CGFloat = 0, b: CGFloat = 0
+        color.getRed(&r, green: &g, blue: &b, alpha: nil)
+        return [r, g, b].map { String(format: "%02lX", Int($0 * 255)) }.reduce("#", +)
     }
     
     /// Gets the color of the pixel localized in the center of the image
